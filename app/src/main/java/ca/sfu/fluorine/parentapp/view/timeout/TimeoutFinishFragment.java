fa808fc8cf74faae124018e1bf056f5140204c0e
Viet Hung Nguyen <viet_hung_nguyen@sfu.ca>
2021-10-28 06:50:57 -0700

    Set up new fragment when the timer reaches 0

---
+++ app/src/main/java/ca/sfu/fluorine/parentapp/view/timeout/TimeoutFinishFragment.java
@@ -0,0 +1,39 @@
+package ca.sfu.fluorine.parentapp.view.timeout;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import ca.sfu.fluorine.parentapp.R;
+import ca.sfu.fluorine.parentapp.databinding.FragmentTimeoutFinishBinding;
+
+public class TimeoutFinishFragment extends Fragment {
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater,
+							 ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return FragmentTimeoutFinishBinding
+				.inflate(inflater, container, false)
+				.getRoot();
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		// Add listener to the button
+		Button dismissButton = FragmentTimeoutFinishBinding.bind(view).button;
+		dismissButton.setOnClickListener(btnView ->
+			Navigation.findNavController(view).navigate(R.id.return_to_timeout)
+		);
+	}
+}
