0d8bb78bef346395d8bcbf1692b37d544921252d
enigma-3 <gavin.98@hotmail.com>
2021-10-28 23:17:37 -0700

    Created foundation for coin flip

---
+++ app/src/main/java/ca/sfu/fluorine/parentapp/CoinFlip.java
@@ -0,0 +1,65 @@
+package ca.sfu.fluorine.parentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class CoinFlip extends AppCompatActivity {
+
+    public static final Random RANDOM = new Random();
+    private TextView coin;
+    private Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_flip);
+        coin = (TextView) findViewById(R.id.coin);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flipCoin();
+            }
+        });
+    }
+
+    private void flipCoin() {
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setDuration(1000);
+        fadeOut.setFillAfter(true);
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                coin.setText(RANDOM.nextFloat() > 0.5f ? "Tails" : "Heads");
+
+                Animation fadeIn = new AlphaAnimation(0, 1);
+                fadeIn.setInterpolator(new DecelerateInterpolator());
+                fadeIn.setDuration(3000);
+                fadeIn.setFillAfter(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
