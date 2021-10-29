0bba3e6895d1f204c691b74d3e918cf800701852
Gavin Nagra <gsnagra@sfu.ca>
2021-10-29 14:30:57 -0700

    Added in FAB for CoinFlipFragment

--- app/src/main/java/ca/sfu/fluorine/parentapp/view/CoinFlipFragment.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/view/CoinFlipFragment.java
@@ -1,15 +1,17 @@
 package ca.sfu.fluorine.parentapp.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import ca.sfu.fluorine.parentapp.databinding.FragmentChildrenBinding;
+import ca.sfu.fluorine.parentapp.CoinFlipActivity;
 import ca.sfu.fluorine.parentapp.databinding.FragmentCoinFlipBinding;
 
 
@@ -22,6 +24,18 @@ public class CoinFlipFragment extends Fragment {
 		// Inflate the layout for this fragment
 		binding = FragmentCoinFlipBinding.inflate(inflater, container, false);
 		return binding.getRoot();
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		binding.navigationCoinFlip.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent add = new Intent(getContext(), CoinFlipActivity.class);
+				startActivity(add);
+			}
+		});
 	}
 
 	@Override
