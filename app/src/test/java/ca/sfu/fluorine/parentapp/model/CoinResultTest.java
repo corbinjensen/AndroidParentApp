d01acd901344c23d63d50d47c317a9fada42b41a
Grace Humer <ghumer@sfu.ca>
2021-10-29 14:10:59 -0700

    Update to test suite (Still need to implement Child funtionality)

--- app/src/test/java/ca/sfu/fluorine/parentapp/model/CoinResultTest.java
+++ app/src/test/java/ca/sfu/fluorine/parentapp/model/CoinResultTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 
 class CoinResultTest {
+    String headString = "HEADS";
+    String tailString = "TAILS";
 
     @Test
     public void getDateTimeOfFlip() {
@@ -19,7 +21,7 @@ class CoinResultTest {
     @Test
     public void getFlipResult() {
         CoinResult expectedCoinResult = new CoinResult();
-        String actualFlipResult = "HEADS";
+        String actualFlipResult = headString;
 
         expectedCoinResult.flipResult = actualFlipResult;
         assertEquals(expectedCoinResult.getFlipResult(), actualFlipResult);
@@ -28,11 +30,12 @@ class CoinResultTest {
     @Test
     public void setFlipResult() {
         CoinResult expectedCoinResult = new CoinResult();
-        boolean flipResult = true;
-        String headsResult = "HEADS";
-        expectedCoinResult.setFlipResult(flipResult);
 
-       assertEquals(expectedCoinResult.flipResult, headsResult);
+        expectedCoinResult.setFlipResult();
+
+
+       assertTrue(expectedCoinResult.flipResult.equals(headString)
+               || expectedCoinResult.flipResult.equals(tailString) );
 
     }
 
@@ -49,11 +52,66 @@ class CoinResultTest {
     @Test
     public void setDidPickerWin() {
         CoinResult expectedCoinResult = new CoinResult();
-        String kidGuess = "HEADS";
+        String kidGuess = headString;
         expectedCoinResult.flipResult = kidGuess;
-        expectedCoinResult.setDidPickerWin(kidGuess);
+        expectedCoinResult.setDidPickerWin();
 
         assertEquals(expectedCoinResult.didPickerWin, true);
+
+    }
+
+    @Test
+    public void setCoinResult() {
+        CoinResult expectedCoinResult = new CoinResult();
+        Child child = new Child(); // TODO : Add init & loose data for child.
+        expectedCoinResult.setCoinResult(child);
+
+        assertTrue(expectedCoinResult.flipResult == headString
+                || expectedCoinResult.flipResult == tailString);
+        assertTrue(expectedCoinResult.didPickerWin == true
+                || expectedCoinResult.didPickerWin == false);
+        assertTrue(expectedCoinResult.childGuess == headString
+                || expectedCoinResult.childGuess == tailString);
+        assertTrue(expectedCoinResult.whoPicked, child); // TODO : CHILD
+
+
+
+    }
+
+    @Test
+    public void getCoinResult(){
+        CoinResult expectedCoinResult = new CoinResult();
+        Child child = new Child();
+        child.first = "FIRST";
+        child.last = "LAST";
+
+        expectedCoinResult.setFlipResult();
+        expectedCoinResult.setDidPickerWin(child);
+        expectedCoinResult.setChildGuess(child);
+
+        assertEquals(expectedCoinResult.getFlipResult(), expectedCoinResult.flipResult);
+        assertEquals(expectedCoinResult.getIfPickerWon(), expectedCoinResult.didPickerWin);
+        assertEquals(expectedCoinResult.getChildGuess(), expectedCoinResult.childGuess);
+
+    }
+
+    @Test
+    public void setChildGuess(){
+        CoinResult coinResult = new CoinResult();
+        String flipGuess = headString;
+        coinResult.setChildGuess(flipGuess);
+
+        assertEquals(coinResult.childGuess, flipGuess);
+
+    }
+
+    @Test
+    public void getChildGuess(){
+        CoinResult coinResult = new CoinResult();
+        String flipGuess = tailString;
+        coinResult.childGuess = flipGuess;
+
+        assertEquals(coinResult.getChildGuess(), flipGuess);
 
     }
 }
