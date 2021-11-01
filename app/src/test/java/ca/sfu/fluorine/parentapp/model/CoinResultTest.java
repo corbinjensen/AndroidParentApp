7433420fa742f6b6f0ebb854619ce42098b01090
Grace Humer <ghumer@sfu.ca>
2021-10-31 22:02:21 -0700

    Remove unnecessarily repetitive individual assignment/creation of the tested CoinResult object.

--- app/src/test/java/ca/sfu/fluorine/parentapp/model/CoinResultTest.java
+++ app/src/test/java/ca/sfu/fluorine/parentapp/model/CoinResultTest.java
@@ -13,13 +13,13 @@ class CoinResultTest {
     private final String TEST_CHILD_FIRST = "FIRST";
     private final String TEST_CHILD_LAST = "LAST";
 
+    Child dummyChild = new Child(TEST_CHILD_FIRST, TEST_CHILD_LAST);
+    CoinSide dummyChildGuess = CoinSide.HEAD;
+    CoinSide dummyFlipResult = CoinSide.TAIL;
+    CoinResult expectedCoinResult = new CoinResult(dummyChild, dummyChildGuess, dummyFlipResult);
+
     @Test
     public void CoinResult(){
-        Child dummyChild = new Child(TEST_CHILD_FIRST, TEST_CHILD_LAST);
-        CoinSide dummyChildGuess = CoinSide.HEAD;
-        CoinSide dummyFlipResult = CoinSide.TAIL;
-        CoinResult expectedCoinResult = new CoinResult(dummyChild, dummyChildGuess, dummyFlipResult);
-
         assertNotNull(expectedCoinResult.getDateTimeOfFlip());
         assertEquals(expectedCoinResult.getWhoPicked(), dummyChild);
         assertEquals(expectedCoinResult.getChildGuess(), dummyChildGuess);
@@ -31,51 +31,26 @@ class CoinResultTest {
 
     @Test
     public void getDateTimeOfFlip() {
-        Child dummyChild = new Child(TEST_CHILD_FIRST, TEST_CHILD_LAST);
-        CoinSide dummyChildGuess = CoinSide.HEAD;
-        CoinSide dummyFlipResult = CoinSide.TAIL;
-        CoinResult expectedCoinResult = new CoinResult(dummyChild, dummyChildGuess, dummyFlipResult);
-
         assertNotNull(expectedCoinResult.getDateTimeOfFlip());
     }
 
     @Test
     public void getFlipResult() {
-        Child dummyChild = new Child(TEST_CHILD_FIRST, TEST_CHILD_LAST);
-        CoinSide dummyChildGuess = CoinSide.HEAD;
-        CoinSide dummyFlipResult = CoinSide.TAIL;
-        CoinResult expectedCoinResult = new CoinResult(dummyChild, dummyChildGuess, dummyFlipResult);
-
         assertEquals(expectedCoinResult.getFlipResult(), dummyFlipResult);
     }
 
     @Test
     public void getChildGuess() {
-        Child dummyChild = new Child(TEST_CHILD_FIRST, TEST_CHILD_LAST);
-        CoinSide dummyChildGuess = CoinSide.HEAD;
-        CoinSide dummyFlipResult = CoinSide.TAIL;
-        CoinResult expectedCoinResult = new CoinResult(dummyChild, dummyChildGuess, dummyFlipResult);
-
         assertEquals(expectedCoinResult.getChildGuess(),dummyChildGuess);
     }
 
     @Test
     public void getWhoPicked() {
-        Child dummyChild = new Child(TEST_CHILD_FIRST, TEST_CHILD_LAST);
-        CoinSide dummyChildGuess = CoinSide.HEAD;
-        CoinSide dummyFlipResult = CoinSide.TAIL;
-        CoinResult expectedCoinResult = new CoinResult(dummyChild, dummyChildGuess, dummyFlipResult);
-
         assertEquals(expectedCoinResult.getWhoPicked(), dummyChild);
     }
 
     @Test
     public void getDidPickerWin() {
-        Child dummyChild = new Child(TEST_CHILD_FIRST, TEST_CHILD_LAST);
-        CoinSide dummyChildGuess = CoinSide.HEAD;
-        CoinSide dummyFlipResult = CoinSide.TAIL;
-        CoinResult expectedCoinResult = new CoinResult(dummyChild, dummyChildGuess, dummyFlipResult);
-
         assertEquals(expectedCoinResult.didPickerWin(), (dummyChildGuess.equals(dummyFlipResult)));
 
     }
