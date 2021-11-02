209bfc1596869961849a0c1c9f7d2b30a0b0616a
Viet Hung Nguyen <viet_hung_nguyen@sfu.ca>
2021-11-02 14:48:05 -0700

    Add test cases for children

--- app/src/main/java/ca/sfu/fluorine/parentapp/model/children/Child.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/model/children/Child.java
@@ -1,6 +1,6 @@
 package ca.sfu.fluorine.parentapp.model.children;
 
-import java.util.ArrayList;
+import androidx.annotation.NonNull;
 
 /**
  *  Child.java - represents a child
@@ -9,7 +9,10 @@ public class Child {
     private final String firstName;
     private final String lastName;
 
-    public Child(String firstName, String lastName) {
+    public Child(@NonNull String firstName, @NonNull String lastName) {
+        if (firstName.isEmpty() || lastName.isEmpty()) {
+            throw new IllegalArgumentException("Input name should not be empty");
+        }
         this.firstName = firstName;
         this.lastName = lastName;
     }
