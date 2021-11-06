df6d5cef51f2aaba3ba5bcfd74a45c29962f7b53
Viet Hung Nguyen <viet_hung_nguyen@sfu.ca>
2021-11-05 22:54:13 -0700

    Add up button to return previous activity

--- app/src/main/java/ca/sfu/fluorine/parentapp/CoinFlipActivity.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/CoinFlipActivity.java
@@ -1,19 +1,9 @@
 package ca.sfu.fluorine.parentapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.animation.Animator;
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
-import android.os.Bundle;
-import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.Button;
-import android.widget.ImageView;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 public class CoinFlipActivity extends AppCompatActivity {
     @Override
@@ -21,10 +11,10 @@ public class CoinFlipActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin_flip);
 
+        // Set up the action bar
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setTitle("New coin flip");
-            actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
-  }
+}
