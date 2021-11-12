d7030d9fd71c465cba8f605c64434af65332874d
Viet Hung Nguyen <viet_hung_nguyen@sfu.ca>
2021-11-11 20:43:39 -0800

    Migrate Child model to Room entity

--- app/src/main/java/ca/sfu/fluorine/parentapp/model/children/Child.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/model/children/Child.java
@@ -1,20 +1,39 @@
 package ca.sfu.fluorine.parentapp.model.children;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 /**
  *  Child.java - represents a child
  */
+@Entity(tableName = "children")
 public class Child {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "first_name")
     private final String firstName;
+
+    @ColumnInfo(name = "last_name")
     private final String lastName;
 
+    @ColumnInfo(name = "created_time")
+    private final long createdTime;
+
     public Child(@NonNull String firstName, @NonNull String lastName) {
-        if (firstName.isEmpty() || lastName.isEmpty()) {
-            throw new IllegalArgumentException("Input name should not be empty");
-        }
         this.firstName = firstName;
         this.lastName = lastName;
+        this.createdTime = System.currentTimeMillis(); // Current UNIX time
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -23,5 +42,9 @@ public class Child {
 
     public String getLastName() {
         return lastName;
+    }
+
+    public long getCreatedTime() {
+        return createdTime;
     }
 }
---
+++ app/src/main/java/ca/sfu/fluorine/parentapp/model/children/ChildDao.java
@@ -0,0 +1,25 @@
+package ca.sfu.fluorine.parentapp.model.children;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+/**
+ * Represents data access point for children data
+ */
+public abstract class ChildDao {
+	@Insert
+	public abstract void addChild(Child child);
+
+	@Delete
+	public abstract void deleteChild(Child child);
+
+	@Query("SELECT * FROM children WHERE id = :id LIMIT 1")
+	public abstract List<Child> getChildById(int id);
+
+	@Update
+	public abstract void updateChild(Child child);
+}
