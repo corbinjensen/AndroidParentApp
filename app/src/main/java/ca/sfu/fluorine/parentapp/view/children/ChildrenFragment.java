a5365613b0e40ce021b97860df183f9e82ff0b2e
Corbin Jensen <corbinjensen@users.noreply.github.com>
2021-11-11 18:55:46 -0800

    Created a new activity called Help Activity to host the help page. Linked the help menu that Viet put into place. Tested by running the app.

--- app/src/main/java/ca/sfu/fluorine/parentapp/view/children/ChildrenFragment.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/view/children/ChildrenFragment.java
@@ -36,7 +36,11 @@ public class ChildrenFragment extends Fragment {
         Bundle savedInstanceState
     ) {
 		// Inflate the layout for this fragment
-		binding = FragmentChildrenBinding.inflate(inflater, container, false);
+		binding = FragmentChildrenBinding.inflate(
+		    inflater,
+            container,
+            false
+        );
 		return binding.getRoot();
 
     }
