8efa25e050032e8cf78da31a9b2a4d193d0d56e9
Corbin Jensen <corbinjensen@users.noreply.github.com>
2021-11-12 20:45:05 -0800

    Links are now clickable, using view binding. Help page is complete as per the story, easy to add more content later as we need.

--- app/src/main/java/ca/sfu/fluorine/parentapp/HelpActivity.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/HelpActivity.java
@@ -3,13 +3,26 @@ package ca.sfu.fluorine.parentapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
+import ca.sfu.fluorine.parentapp.databinding.ActivityHelpBinding;
+
 public class HelpActivity extends AppCompatActivity {
+    private ActivityHelpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
+        binding = ActivityHelpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.appIconCitation.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.childIconCitation.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.coinIconCitation.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.pulsatorCitation.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.taskIconCitation.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.timerIconCitation.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 }
