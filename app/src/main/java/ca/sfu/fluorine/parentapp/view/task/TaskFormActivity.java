4a83b5b6e6345434b8e741aa47b857755489ed8b
enigma-3 <gavin.98@hotmail.com>
2021-11-15 15:16:36 -0800

    Made UI for task form

--- app/src/main/java/ca/sfu/fluorine/parentapp/view/task/TaskFormActivity.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/view/task/TaskFormActivity.java
@@ -3,19 +3,40 @@ package ca.sfu.fluorine.parentapp.view.task;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import java.util.List;
+
+import ca.sfu.fluorine.parentapp.R;
 import ca.sfu.fluorine.parentapp.databinding.ActivityTaskFormBinding;
+import ca.sfu.fluorine.parentapp.model.AppDatabase;
+import ca.sfu.fluorine.parentapp.model.children.Child;
+import ca.sfu.fluorine.parentapp.model.task.Task;
+import ca.sfu.fluorine.parentapp.model.task.TaskAndChild;
 
 public class TaskFormActivity extends AppCompatActivity {
+    private static final String CHILD_ID = "childIndex";
+    public static final int ADD_TASK = 0;
+    public static final int ADD_CHILD = -1;
 
     private ActivityTaskFormBinding binding;
-    public static final int ADD_TASK = 0;
+    private AppDatabase database;
+    private Task task = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTaskFormBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-    }
+        setTitle("New Task");
 
+        database = AppDatabase.getInstance(getApplicationContext());
+
+        List<Task> tasks = database.taskDao().getChildById(
+                getIntent().getIntExtra(CHILD_ID, ADD_TASK)
+        );
+
+
+
+    }
 }
