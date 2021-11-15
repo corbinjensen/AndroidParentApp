14439209aff5f442fa5545740eb774a71ec56108
Corbin Jensen <corbinjensen@users.noreply.github.com>
2021-11-14 17:36:21 -0800

    Set up the floating action button and a TaskFormActivity to Navigate to

--- app/src/main/java/ca/sfu/fluorine/parentapp/view/task/TaskListFragment.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/view/task/TaskListFragment.java
@@ -3,6 +3,7 @@ package ca.sfu.fluorine.parentapp.view.task;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 
 import ca.sfu.fluorine.parentapp.R;
 import ca.sfu.fluorine.parentapp.databinding.FragmentTaskListBinding;
+import ca.sfu.fluorine.parentapp.view.children.ChildFormActivity;
 
 /**
  * TaskListFragment.java - Represents a home screen and feed/list of tasks
@@ -18,7 +20,14 @@ import ca.sfu.fluorine.parentapp.databinding.FragmentTaskListBinding;
  */
 public class TaskListFragment extends Fragment {
 
-	@Override
+    private FragmentTaskListBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
@@ -26,4 +35,20 @@ public class TaskListFragment extends Fragment {
 				.inflate(inflater, container, false);
 		return binding.getRoot();
 	}
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // floating action button
+        // TODO add Intent to new taskForm Activity
+        binding.buttonAddTask.setOnClickListener(
+            btnView -> {
+                startActivity(
+                    ChildFormActivity.makeIntent(
+                        requireContext(),
+                        TaskFormActivity.ADD_TASK
+                    )
+                );
+            });
+    }
 }
