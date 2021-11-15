40a8d944198b692741cc5b0dc3813800a5b95c70
Corbin Jensen <corbinjensen@users.noreply.github.com>
2021-11-14 23:50:50 -0800

    Removed dead code

--- app/src/main/java/ca/sfu/fluorine/parentapp/view/task/TaskListFragment.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/view/task/TaskListFragment.java
@@ -60,25 +60,5 @@ public class TaskListFragment extends Fragment {
         binding = null;
     }
 
-    // TODO Tie this in with the Task Model
-    /*
-    class TaskListAdapter extends RecyclerView.Adapter<TaskViewHolder> {
 
-        @NonNull
-        @Override
-        public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 0;
-        }
-    }
-     */
 }
