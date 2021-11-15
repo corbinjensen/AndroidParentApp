8c45a2225ea19a4119c14b75a1694f6553895345
Corbin Jensen <corbinjensen@users.noreply.github.com>
2021-11-15 14:24:20 -0800

    commit before merge

--- app/src/main/java/ca/sfu/fluorine/parentapp/view/task/TaskListFragment.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/view/task/TaskListFragment.java
@@ -12,7 +12,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import ca.sfu.fluorine.parentapp.R;
 import ca.sfu.fluorine.parentapp.databinding.FragmentTaskListBinding;
+import ca.sfu.fluorine.parentapp.model.AppDatabase;
+import ca.sfu.fluorine.parentapp.model.children.Child;
+import ca.sfu.fluorine.parentapp.model.task.TaskAndChild;
+import ca.sfu.fluorine.parentapp.view.children.ChildFormActivity;
+import ca.sfu.fluorine.parentapp.view.children.ChildViewHolder;
 import ca.sfu.fluorine.parentapp.view.coin.CoinFlipActivity;
 
 /**
@@ -20,6 +28,7 @@ import ca.sfu.fluorine.parentapp.view.coin.CoinFlipActivity;
  *  user can create a new task, or edit existing ones.
  */
 public class TaskListFragment extends Fragment {
+    private AppDatabase database;
 
     private FragmentTaskListBinding binding;
 
@@ -58,6 +67,53 @@ public class TaskListFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    class TaskListAdapter extends RecyclerView.Adapter<TaskViewHolder> {
+        private final List<TaskAndChild> tasks;
+
+        public TaskListAdapter() {
+            tasks = database.taskDao().getAllTasksWithChildren();
+        }
+
+        @NonNull
+        @Override
+        public TaskViewHolder onCreateViewHolder(
+            @NonNull
+                ViewGroup parent,
+            int viewType
+        ) {
+            View view = LayoutInflater.from(requireContext()).inflate(
+                R.layout.task_row_layout,
+                parent,
+                false
+            );
+            return new TaskViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(
+            @NonNull
+                ChildViewHolder holder,
+            int position
+        ) {
+            // get child object from index
+            TaskAndChild task = tasks.get(position);
+
+            // change the text to display child name
+            holder.taskNameDisplay.setText(task.getTask().getName());
+
+            // make the list item clickable
+            holder.itemView.setOnClickListener((View view) -> {
+                Intent intent = ChildFormActivity.makeIntent(requireContext(), task.getTask().getId());
+                startActivity(intent);
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return tasks.size();
+        }
     }
 
 
