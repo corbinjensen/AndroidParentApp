d1d97adf123c8f09bd1595acf081d46561df5732
enigma-3 <gavin.98@hotmail.com>
2021-11-16 21:52:00 -0800

    Add task is fully functional, and now edit task foundation has been created

--- app/src/main/java/ca/sfu/fluorine/parentapp/view/task/EditTaskActivity.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/view/task/EditTaskActivity.java
@@ -1,10 +1,14 @@
 package ca.sfu.fluorine.parentapp.view.task;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
@@ -12,6 +16,7 @@ import java.util.List;
 import ca.sfu.fluorine.parentapp.R;
 import ca.sfu.fluorine.parentapp.databinding.ActivityTaskFormBinding;
 import ca.sfu.fluorine.parentapp.model.AppDatabase;
+import ca.sfu.fluorine.parentapp.model.task.Task;
 import ca.sfu.fluorine.parentapp.model.task.TaskAndChild;
 
 public class EditTaskActivity extends AddTaskActivity {
@@ -26,37 +31,52 @@ public class EditTaskActivity extends AddTaskActivity {
         // Get the task id from intent then fetch from database
         List<TaskAndChild> tasks = database.taskDao().getTaskByIdWithChild(getIntent()
                 .getIntExtra(TASK_ID, 0));
-        taskAndChild = tasks.get(0);
 
+        taskAndChild = tasks.get(0);
         binding.editTaskName.setText(task.getTask().getName());
-        binding.buttonDeleteTask.setVisibility(View.VISIBLE);
-        setTitle("Edit Child");
-
-        taskAndChild = tasks.get(0);
-        binding.editTaskName.setText(taskAndChild.getTask().getName());
-
+        binding.dropdownSelection.setText(task.getChild().getId());
         // TODO: Add edit option for child selection
 
-        binding.buttonSaveTask.setOnClickListener(editChildrenDialogListener);
+        setTitle("Edit Task");
+        binding.buttonSaveTask.setOnClickListener(editTaskDialogListener);
         binding.buttonDeleteTask.setVisibility(View.VISIBLE);
-        binding.buttonDeleteTask.setOnClickListener(deleteChildDialogListener);
+        binding.buttonDeleteTask.setOnClickListener(deleteTaskDialogListener);
 
         binding.editTaskName.addTextChangedListener(watcher);
         binding.dropdownSelection.addTextChangedListener(watcher);
     }
 
     // TODO: Fix update task & child for task
-    private final View.OnClickListener editChildrenDialogListener = (btnView) -> makeConfirmDialog(
-            R.string.edit_child,
-            R.string.edit_child_confirm,
+    private final View.OnClickListener editTaskDialogListener = (btnView) -> makeConfirmDialog(
+            R.string.edit_task,
+            R.string.edit_task_confirm,
             (dialogInterface, i) -> {
                 String taskName = binding.editTaskName.getText().toString();
-                String lastName = binding.editTextLastName.getText().toString();
-                child.updateName(firstName, lastName);
-                persistIconData(child);
-                database.childDao().updateChild(child);
+                String selection = binding.dropdownMenu.getTransitionName().toString();
+               // task.updateTask( , );
+                database.taskDao().updateTask(taskAndChild.getTask());
                 finish();
             });
+
+    private final View.OnClickListener deleteTaskDialogListener = (btnView) -> makeConfirmDialog(
+            R.string.delete_task,
+            R.string.delete_child_confirm,
+            (dialogInterface, i) -> {
+                database.taskDao().deleteTask(taskAndChild.getTask());
+                finish();
+            });
+
+    private void makeConfirmDialog(@StringRes int titleId,
+                                   @StringRes int messageId,
+                                   @NonNull DialogInterface.OnClickListener confirmAction) {
+        new AlertDialog.Builder(this)
+                .setTitle(titleId)
+                .setMessage(messageId)
+                .setCancelable(false)
+                .setPositiveButton(android.R.string.ok, confirmAction)
+                .setNegativeButton(android.R.string.cancel, (dialog, i) -> dialog.dismiss())
+                .show();
+    }
 
     private final TextWatcher watcher = new TextWatcher() {
         @Override
