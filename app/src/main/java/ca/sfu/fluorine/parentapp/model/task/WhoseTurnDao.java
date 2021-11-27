8c1557e2fffc94075859a68b7c480e7b43a637b6
Viet Hung Nguyen <viet_hung_nguyen@sfu.ca>
2021-11-26 21:25:42 -0800

    Implement DAO to access the task history

---
+++ app/src/main/java/ca/sfu/fluorine/parentapp/model/task/WhoseTurnDao.java
@@ -0,0 +1,20 @@
+package ca.sfu.fluorine.parentapp.model.task;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import java.util.List;
+
+import ca.sfu.fluorine.parentapp.model.BaseDao;
+import ca.sfu.fluorine.parentapp.model.composite.WhoseTurnRecord;
+
+@Dao
+public abstract class WhoseTurnDao implements BaseDao<WhoseTurn> {
+    @Query("SELECT children.*, completionTime FROM whose_turn " +
+            "JOIN children ON child_id = assigned_child_id " +
+            "JOIN tasks ON task_id = assigned_task_id " +
+            "WHERE assigned_child_id = :taskId " +
+            "ORDER BY completionTime DESC")
+    public abstract LiveData<List<WhoseTurnRecord>> getAllWhoseTurnHistoryFrom(int taskId);
+}
