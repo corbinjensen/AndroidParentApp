505508dc7d2fe5ae0c1163800c4ff129a5c255a6
Grace Humer <ghumer@sfu.ca>
2021-11-27 18:37:02 -0800

    Add function getProgressInt to get an integer representaion of how much time is elapsed, on scale from 0-100.

--- app/src/main/java/ca/sfu/fluorine/parentapp/viewmodel/timeout/TimeoutViewModel.java
+++ app/src/main/java/ca/sfu/fluorine/parentapp/viewmodel/timeout/TimeoutViewModel.java
@@ -44,6 +44,18 @@ public class TimeoutViewModel extends AndroidViewModel {
         return timerState;
     }
 
+    //Gives an int to represent time elapsed since timer's creation (on scale low:0-high:100)
+    public int getProgressInt(){
+        long totalMillis = totalDuration / 100 ;
+        long currentMillis = 0;
+
+        if (millisLeft.getValue() != null){
+            currentMillis = millisLeft.getValue();
+        }
+        int progressCounter = (int) (currentMillis / totalMillis);
+        return (100 - progressCounter);
+    }
+
     // Methods for the views interact with this view model
     public void resumeTimer() {
         makeTimerFromSettings();
