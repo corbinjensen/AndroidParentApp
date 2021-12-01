ce845b78ac0b97970a107aad8ba5f76752d02f64
Viet Hung Nguyen <viet_hung_nguyen@sfu.ca>
2021-12-01 01:44:52 -0800

    Implement the storage and state for breathing screen

---
+++ app/src/main/java/ca/sfu/fluorine/parentapp/store/BreathingStorage.java
@@ -0,0 +1,34 @@
+package ca.sfu.fluorine.parentapp.store;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.scopes.ActivityRetainedScoped;
+
+@ActivityRetainedScoped
+public class BreathingStorage {
+    private static final String KEY = "breathing";
+    private static final String BREATH_COUNT = "breath_count";
+    private static final int DEFAULT_BREATH_COUNT = 5;
+
+    private final SharedPreferences preferences;
+
+    @Inject
+    public BreathingStorage(@NonNull Application application) {
+        preferences = application
+                .getApplicationContext()
+                .getSharedPreferences(KEY, Application.MODE_PRIVATE);
+    }
+
+    public void storeBreathCount(int breathCount) {
+        preferences.edit().putInt(BREATH_COUNT, breathCount).apply();
+    }
+
+    public int getBreathCount() {
+        return preferences.getInt(BREATH_COUNT, DEFAULT_BREATH_COUNT);
+    }
+}
---
+++ app/src/main/java/ca/sfu/fluorine/parentapp/viewmodel/BreathingState.java
@@ -0,0 +1,40 @@
+package ca.sfu.fluorine.parentapp.viewmodel;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.function.Consumer;
+
+/**
+ * Represent the data object of the the state
+ *
+ * The UI will change if new state replace the old state
+ */
+public class BreathingState {
+    // Make the breathing state exposed to the interface
+    // However, changing the parameters with the instance won't affect the UI
+    boolean isBreathingBegin = false;
+    int breathingInOut;
+    boolean isButtonPressingTooLong = false;
+    boolean isBreathingFinish = false;
+
+    private BreathingState() {}
+
+    public BreathingState(int breathCount) {
+        breathingInOut = breathCount * 2;
+    }
+
+    // Return new instance of BreathingState with a few modifications
+    public BreathingState change(@NonNull BreathingState oldState,
+                               @Nullable Consumer<BreathingState> stateModifier) {
+        BreathingState newState = new BreathingState();
+        newState.isBreathingBegin = oldState.isBreathingBegin;
+        newState.breathingInOut = oldState.breathingInOut;
+        newState.isButtonPressingTooLong = oldState.isButtonPressingTooLong;
+        newState.isBreathingFinish = oldState.isBreathingFinish;
+        if (stateModifier != null) {
+            stateModifier.accept(newState);
+        }
+        return newState;
+    }
+}
